24305ee62d8c165e2fcf33de02588b13c4a7f908
Michael Grote <93448865+michi3214@users.noreply.github.com>
2023-04-28 13:40:24 +0200

    Add function names and docstrings for database

--- Database/Database.js
+++ Database/Database.js
@@ -1,16 +1,27 @@
 /**
- * This script implement all functions with connection to database. 
+ * This script implement all functions for connection to database. 
 */
-
-//TODO Nicht alle Funktionen sind bereits umgestellt. 
 const mysql = require("mysql2/promise");
 const DATABASE = require("constant_Database");
 const { database } = require("./constant_Database");
 
+
+/** 
+ * create the connection to the database and use the data from the constant_Database.
+ * 
+ * Please use this function only in a try and catch
+ * @returns {mysql.Connection} connection
+ */
 async function _create_Connection(){
-	return mysql.createConnection({host: DATABASE.host, user:DATABASE.user , database: DATABASE.database});
+	return await mysql.createConnection({host: DATABASE.host, user:DATABASE.user , database: DATABASE.database});
 }
 
+
+/**
+ * Read all columns from the given table.
+ * @param {String} tbl_name - name of the table, which should be read
+ * @returns { mysql.RowDataPacket | String} columns, on error: errorMsg
+ */
 async function _load_from_database(tbl_name){
 	try {
 		const connection = await _create_Connection(); 
@@ -18,10 +29,13 @@ async function _load_from_database(tbl_name){
 		const [rows] = await connection.execute(select_string);
 		return rows;
 	} catch (error) {
-		console.error("Can not load data from " + tbl_name + " database: " + error);
+		const errMsg = "The access to the database failed. Error: \n" + error;
+		console.error("_load_from_database: " + errMsg);
+		return errMsg;
 	}
 }
 
+//TODO -[] Create docstring 
 async function _insert_to_database(tbl_name,columns ,content){
 	try {
 		const connection = await _create_Connection();
@@ -38,25 +52,36 @@ async function load_content_from_database (){
 	return _load_from_database (tbl_name_content);
 }
 
-async function add_content_to_database(content){
-	const tbl_name_content = database.tbl_names.tbl_content;
-	await _insert_to_database(tbl_name_content, ());
-	return 1;
+//TODO implement function and docstring
+async function add_content_to_database (){
+	
 }
 
-async function delete_note_from_database(id){
-	try {
-		const connection = await mysql.createConnection({host:"localhost", user: "root", database: "tinf21C_webtechnologien"});
-		await connection.execute("DELETE FROM `tbl_notes` WHERE (id=?);", [id]); // array wird mit for durchgelaufen und einzeln eingefügt DELETE FROM table_name WHERE condition;
-		console.log("You deleted the note with the id:", id);
-	} catch (error) {
-		console.error("Failed to delete note");
-		console.error(error);
-	}
+//TODO implement function and docstring
+async function delete_content_from_database (){
+	
 }
+
+//TODO implement function and docstring
+async function load_user_from_database (){
+}
+
+//TODO implement function and docstring
+async function add_user_to_database (){
+	
+}
+
+//TODO implement function and docstring
+async function delete_user_from_database (){
+	
+}
+
 
 module.exports =  {
 	load_content_from_database,
 	add_content_to_database,
-	delete_note_from_database
+	delete_content_from_database,
+	load_user_from_database,
+	add_user_to_database,
+	delete_user_from_database
 };
